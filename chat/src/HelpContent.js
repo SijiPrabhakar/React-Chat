be65aed79dd2d1131546c6d6a27d17f1f8ca751c
SijiPrabhakar <sijiprabhakarn@gmail.com>
2020-08-13 12:56:56 +0000

    addedhelp

--- chat/src/HelpContent.js
+++ chat/src/HelpContent.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
-import "./GroupChat.css";
+import "./HelpContent.css";
 
 class HelpContent extends React.Component {
       
@@ -12,8 +12,45 @@ class HelpContent extends React.Component {
   render() {
     return (
       <div className="content">
-        <h4>NOTE: Please check the below keyword if you find difficulties in choosing the request.</h4>
-
+        <h4>NOTE: Please check the below keyword if you find difficulties in choosing the request.</h4><br/>
+        <p><strong>Overview of keywords :</strong></p>
+        <p>
+            1. Infosys related<br/>
+            2. Charter related<br/>
+            3. ICOMS overview<br/>
+        </p>
+        <p><strong>INFOSYS RELATED:</strong></p>
+        <p>
+            1. Adding Mail IDs to DL <br/>
+            2. Cubicle or Desktop or Laptop allocation (Note: you can type any one)<br/>
+            3. Adding URLs to Trusted Sites<br/>
+            4. Allocating ALM asset in AMS<br/>
+        </p>
+        <p><strong>CHARTER RELATED:</strong></p>
+        <p>
+            1. PID Activation<br/>
+            2. Adding Charter Mail to Charter DL<br/>
+            3. ALM request<br/>
+            4. ID request<br/>
+            5. VPN Request<br/>
+            6. Application Access for New Users<br/>
+            7. Trouble Tickets for Existing Users<br/>
+            8. Field Glass<br/>
+            9. Shared Folder Access request<br/>
+            10. NCSS Portal Access request<br/>
+            11. BRF Access request<br/>
+            12. Configuring Charter Outlook<br/>
+        </p>
+        <p><strong>ICOMS OVERVIEW:</strong></p><br/>
+        <p>
+            1. Track Level Overview / Applications Used<br/>
+            2. Billing System<br/>
+            3. Residential<br/>
+            4. Commercial/Business<br/>
+            5. Transaction Supported/Scenario<br/>
+            6. ICOMS/ CRM/ ATLAS (Note: You can type any one request)<br/>
+        </p><br/>
+        <h6>We are still updating to make you feel more comfort while using the chatbot. Thank you for using the application..!</h6>
 
 
       </div>
--- chat/src/MainPage.js
+++ chat/src/MainPage.js
@@ -3,18 +3,31 @@ import "./MainPage.css";
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Link} from 'react-router-dom';
-
+import Timestamp from 'react-timestamp';
 class MainPage extends React.Component {
   
   constructor(props) {
     super(props)
+    var today = new Date(),
+
+    time = today.getHours() ;
     this.state = {
       NewUser: "BOT: Select a choice to proceed, \\n 1. Infosys Related Requests \\n 2. Charter Related Requests \\n 3. ICOMS Application overview \\n Kindly type a valid choice or type quit to exit",
       ExistingUser: "ExistingUser",
+      currentDateTime: Date().toLocaleString(),
+      currentTime: time,
+      greeting: ''
     }
    
   }
   
+  componentDidMount() {
+  //   var today = new Date(),
+  //   time = today.getHours() ;
+  //   greeting: time ===
+  // componentDidUpdate() {
+   
+  }
 
   render() {
     return (
@@ -36,6 +49,11 @@ class MainPage extends React.Component {
                         <button className="button">New User</button>
             </Link>
           </div>
+        
+          <h2>{this.state.currentDateTime}</h2>
+          <div style={{'backgroundColor': status === 'approved' ? 'blue' : status === 'pending' ? 'black' : 'red'}}>
+</div>
+          
          
       </div>
       
